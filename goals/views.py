6413954fbaf4729dc08ed5c8f0f1361f70e966e5
onagbonoga <55672475+onagbonoga@users.noreply.github.com>
2020-06-30 03:06:17 -0500

    Update views.py
    todo items are now updated in databse through to do item form in view goals page

--- goals/views.py
+++ goals/views.py
@@ -1,20 +1,23 @@
 from flask import Flask, Blueprint, url_for, render_template,request, redirect,session
-import pickle
+import json
 
 from app import db
-from goals.models import Goal
+from goals.models import Goal, GoalMod
 from goals.forms import AddGoalForm
 goals_app = Blueprint('Goals', __name__)
 
 @goals_app.route('/view_goals',methods =("GET","POST"))
 def view_goals():
-	allGoals = Goal.query.filter_by(user_id = session['id'])
 	
 	if request.method == "POST":
+		allGoals = Goal.query.filter_by(user_id = session['id'])
+		allGoalsD = deSerialize(allGoals)
 		#x = request.form.getlist('something')
 		#return f"{x}"
 		#keylist = []
-		for eachGoal in allGoals: #checking each object in all goals 
+		i = -1 #for indexing
+		for eachGoal in allGoalsD: #checking each object in all goals 
+			i = i + 1
 			for key in eachGoal.todo.keys(): #checking each key in each todo list
 				keyName = "\""+key+"\""
 				response = request.form.get(keyName)
@@ -22,22 +25,29 @@ def view_goals():
 				#return f"{response}"
 				if response != None:
 					#eachGoalP = pickle.loads( eachGoal.todo)
+					#return f"{type(eachGoal.todo)}"
+					#y = json.loads(eachGoal.todo)
+					#y['keyName'] = True
 					eachGoal.todo[key] = True
+					allGoals[i].todo = json.dumps(eachGoal.todo)
+					#eachGoal.todo = json.dumps(y)
 					
-					db.session.commit()
-					return f"{eachGoal.todo}"
-					
-					
+					#db.session.commit()
+					#return f"{allGoals[i].todo}"
+						
 				else:
 					eachGoal.todo[key] = False
-
+					allGoals[i].todo = json.dumps(eachGoal.todo)
 					#db.session.add(eachGoal)
-			eachGoal.todo = eachGoal.todo
+			
 			db.session.commit()
 
 	low = Goal.query.filter_by(user_id = session['id'], priority = "low")
+	low = deSerialize(low)
 	medium = Goal.query.filter_by(user_id = session['id'], priority = "medium")
+	medium = deSerialize(medium)
 	high = Goal.query.filter_by(user_id = session['id'], priority = "high")
+	high = deSerialize(high)
 	#return f"{low[0].todo}"
 	return render_template('view_goals.html',low = low, medium=medium, high = high)
 
@@ -48,7 +58,7 @@ def add_goals():
 	if form.validate_on_submit():
 		todoItemsList = request.form.getlist("todoItemF")
 		user_id = session['id']
-		todo = parseTodo(todoItemsList)
+		todo = json.dumps(parseTodo(todoItemsList))
 		#todo = pickle.dumps(todo)
 		newGoal = Goal(
 			form.goal.data,
@@ -68,6 +78,17 @@ def parseTodo(todo):
 	for currentItem in todo:
 		result[currentItem] = False
 	return result
+
+def deSerialize(goalList):
+	result = []
+	for eachGoal in goalList:
+		newTodo = json.loads(eachGoal.todo)
+		result.append(GoalMod(eachGoal.goal,
+				eachGoal.priority,
+				newTodo))
+
+	return result
+
 
 
 
